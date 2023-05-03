afcdc31538d82abb09e8ee2cf9a35c36c60cf27b
Simon LEONARD <git-1001af4@sinux.sh>
2023-05-03 19:39:12 +0100

    feat: email working (mock data)

--- functions/send_emails/app.py
+++ functions/send_emails/app.py
@@ -5,5 +5,28 @@ import os
 def lambda_handler(event, context):
     print("mock email sent with", event)
 
+    ses = boto3.client('ses')
+    res = ses.send_email(
+        Source=os.environ['SES_EMAIL'],
+        Destination={
+            'ToAddresses': [
+                # test purpose, won't be change for this assignment
+                "aws-no-reply@sinux.sh"
+            ],
+        },
+        Message={
+            'Subject': {
+                'Data': 'Ticket creation test',
+                'Charset': 'UTF-8'
+            },
+            'Body': {
+                'Text': {
+                    'Data': 'Ticket creation body test',
+                    'Charset': 'UTF-8'
+                }
+            }
+        }
+    )
+
     # return okey to step function as task
     return {}
